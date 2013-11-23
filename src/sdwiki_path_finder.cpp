9c06484e45c83ca1cd3d6c95c581eaf33e8ddd92
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-11-22 23:23:22 -0500

    Read in the links (in a inefficient way)

--- src/sdwiki_path_finder.cpp
+++ src/sdwiki_path_finder.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <iostream>
+#include <sstream>
 #include <fstream>
 #include <vector>
 #include <unordered_map>
@@ -42,6 +43,44 @@ int main(int argc, char* argv[])
 
 		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
 		std::cout << "\033[94m  -> \033[0mtook " << duration << " seconds\n";
+	}
+
+	// Load in all page links into a hash map
+	std::cout << "\033[92m==>\033[0m Reading in page links as a map\n";
+
+	std::unordered_map<int,std::vector<int>> page_links;
+
+	{
+		std::clock_t start = std::clock();
+
+		std::ifstream page_links_file(argv[2]);
+		std::string links;
+
+		while (std::getline(page_links_file, links))
+		{
+			int split_at = links.find(':');
+			int link_id = atoi(links.substr(0, split_at).c_str());
+			links = links.substr(split_at + 2);
+
+			std::stringstream stream_links(links);
+			int target_id;
+
+			while (stream_links >> target_id)
+			{
+				page_links[link_id].push_back(target_id);
+			}
+		}
+
+		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+		std::cout << "\033[94m  -> \033[0mtook " << duration << " seconds\n";
+	}
+
+	int page_id = page_ids[argv[3]];
+	std::cout << argv[3] << "(" << page_id << ") links to:\n";
+
+	for (int i : page_links[page_id])
+	{
+		std::cout << " * " << pages[i] << std::endl;
 	}
 
 	return 0;
