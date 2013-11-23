5b4fda63a97d927ca2110a8efbda6de6540afdb1
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-11-23 04:18:56 -0500

    Extract the loading logic as functions

--- src/sdwiki_path_finder.cpp
+++ src/sdwiki_path_finder.cpp
@@ -9,6 +9,79 @@
 #include <ctime>
 #include <omp.h>
 
+// Typedef for our primary data types
+typedef std::vector<std::string> pages_t;
+typedef std::unordered_map<std::string, int> page_ids_t;
+typedef std::unordered_map<int,std::vector<int>> page_links_t;
+
+/* Load in the page titles and page ids from a file.
+ *
+ * Each line of the file will represent the page ID, starting from line 1 until
+ * the end of the file. The page titles will be stored into the pages vector.
+ * The page titles => ids will be stored in the page_ids map
+ *
+ * Returns: The total number of pages loaded
+ */
+int load_page_titles(const std::string &file_path, pages_t &pages, page_ids_t &page_ids)
+{
+	std::ifstream page_titles_file(file_path);
+	std::string page_name;
+
+	// We won't be using the first index of the vector for anything since
+	// the page_id indexes start at 1 instead of 0. This should help to
+	// prevent any errors during development
+	pages.push_back("");
+
+	// Read the page name into the vector and into the hash-map
+	int index = 1;
+
+	while (std::getline(page_titles_file, page_name))
+	{
+		pages.push_back(page_name);
+		page_ids[page_name] = index++;
+	}
+
+	return index - 1;
+}
+
+/* Load in the page links from a file.
+ *
+ * Each line of the file should start with a page_id that represents a page, it
+ * should then follow with a colon (':'). All numbers after the colon represent
+ * page_ids that that page links to.
+ *
+ * Read all page links into the page_links hash map, where the key is the
+ * page_id and the value is a vector of page_ids the page links to.
+ *
+ * Returns: The total number of links loaded
+ */
+int load_page_links(const std::string &file_path, page_links_t &page_links)
+{
+	std::ifstream page_links_file(file_path);
+	std::string link_id;
+
+	int current_link = 0;
+	int total_links  = 0;
+
+	while (page_links_file >> link_id)
+	{
+		if (link_id.back() == ':')
+		{
+			current_link = atoi(link_id.c_str());
+		}
+		else
+		{
+			page_links[current_link].push_back(atoi(link_id.c_str()));
+			total_links++;
+		}
+	}
+
+	return total_links;
+}
+
+
+
+
 int main(int argc, char* argv[])
 {
 	if (argc != 5)
@@ -17,64 +90,37 @@ int main(int argc, char* argv[])
 		exit(1);
 	}
 
+	// Used to store a list of page titles. Each index represents a page title
+	pages_t pages;
+
+	// Used to reverse lookup a page ID by it's title
+	page_ids_t page_ids;
+
+	// Used to store a list of page_ids that a given page_id links to
+	page_links_t page_links;
+
 	// Load in all page titles into a vector
 	std::cout << "\033[92m==>\033[0m Reading in page titles as a vector & map\n";
 
-	std::vector<std::string> pages;
-	std::unordered_map<std::string, int> page_ids;
-
 	{
 		std::clock_t start = std::clock();
-
-		std::ifstream page_titles_file(argv[1]);
-		std::string page_name;
-
-		// We won't be using the first index of the vector for anything since
-		// the page_id indexes start at 1 instead of 0. This should help to
-		// prevent any errors during development
-		pages.push_back("");
-
-		// Read the page name into the vector and into the hash-map
-		int index = 1;
-
-		while (std::getline(page_titles_file, page_name))
-		{
-			pages.push_back(page_name);
-			page_ids[page_name] = index++;
-		}
-		std::cout << "\033[94m  -> \033[0mRead in " << index - 1 << " page titles\n";
-
+		int total = load_page_titles(std::string(argv[1]), pages, page_ids);
 		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-		std::cout << "\033[94m  -> \033[0mTook " << duration << " seconds\n";
+
+		std::cout << "\033[94m  -> \033[0mRead in " << total    << " page titles\n";
+		std::cout << "\033[94m  -> \033[0mTook "    << duration << " seconds\n";
 	}
 
 	// Load in all page links into a hash map
 	std::cout << "\033[92m==>\033[0m Reading in page links as a map\n";
 
-	std::unordered_map<int,std::vector<int>> page_links;
-
 	{
 		std::clock_t start = std::clock();
-
-		std::ifstream page_links_file(argv[2]);
-		std::string link_id;
-
-		int current_link = 0;
-
-		while (page_links_file >> link_id)
-		{
-			if (link_id.back() == ':')
-			{
-				current_link = atoi(link_id.c_str());
-			}
-			else
-			{
-				page_links[current_link].push_back(atoi(link_id.c_str()));
-			}
-		}
-
+		int total = load_page_links(std::string(argv[2]), page_links);
 		double duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
-		std::cout << "\033[94m  -> \033[0mTook " << duration << " seconds\n";
+
+		std::cout << "\033[94m  -> \033[0mRead in " << total    << " page links\n";
+		std::cout << "\033[94m  -> \033[0mTook "    << duration << " seconds\n";
 	}
 
 	std::cout << "\033[92m==>\033[0m Beginning BFS on Wikipedia graph\n";
